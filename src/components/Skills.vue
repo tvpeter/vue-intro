99beecace21064f597baaedffeb9e7e73cdffc5a
tvpeter <withtvpeter@gmail.com>
2018-12-29 11:10:29 +0100

    Vue setup through cli, collect and display user input

---
+++ src/components/Skills.vue
@@ -0,0 +1,80 @@
+<template>
+   <div class="holder">
+     <form @submit.prevent="addSkill">
+     <input type="text" placeholder="enter a skill" v-model="skill">
+         </form>
+      <ul>
+        <li v-for="(data, index) in skills" v-bind:key="index">
+          {{data.skill}}
+        </li>
+      </ul>
+      <!-- <p v-if="skills.length >= 1">You have more than one skill </p>
+      <p v-else>You have atmost one skill</p> -->
+      <!-- <div v-bind:class="alertObject"></div> -->
+    <p>These are the skills that you have </p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+ data (){
+   return {
+     skill : '',
+     skills: [
+       { "skill" : 'vue js'},
+       { "skill" : 'node js'}
+     ],
+     alertObject: {
+       alert: true
+     }
+   }
+ },
+ methods : {
+   addSkill () {
+     this.skills.push({skill: this.skill})
+     this.skill = '';
+       }
+ }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.holder {
+    background: #fff;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #dddddd;
+    color: #000000;
+  }
+</style>
---
+++ vue-skills.md
@@ -0,0 +1,25 @@
+# Vue Project steps
+
+#### First step 
+
+= Installation using one of the following
+i)  Through cdn using the script tag in the html eg 
+        <body>
+            <script>
+            <script src="https://cdn.jsdelivr.net/npm/vue@2.5.21/dist/vue.js"></script>
+            </script>
+        </body>
+ii) Through npm 
+    npm install vue
+
+iii) Through cli
+    npm install -g @vue/cli
+
+### Create Project using the cli 
+
+ - `vue create projectName`
+
+ - select default for preconfigurations
+ 
+ - `cd ProjectName` and `npm run serve`
+
